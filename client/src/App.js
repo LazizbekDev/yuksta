bd0ba3ccd825050fb22d4175b0324d7087de7fef
LazizbekDev <lazizbektojiboyev0611@gmail.com>
2022-09-17 04:06:41 -0700

    client partition

---
+++ client/src/App.js
@@ -0,0 +1,54 @@
+import { LionPlayer } from 'lion-player';
+import 'lion-player/dist/lion-skin.min.css';
+import "./App.css"
+import {Box, Button, Container, FormControl, Input, InputLabel} from "@mui/material";
+import axios from "axios";
+import {useState} from "react";
+
+function App() {
+    const [link, setLink] = useState([]);
+
+    const submitHandler = async (e) => {
+        e.preventDefault();
+
+        const data = new FormData(e.currentTarget);
+        const url = data.get('url')
+
+        try {
+            const res = await axios.post('http://localhost:5000/api/yuksta', {url}, {
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+
+            setLink(res.data)
+            console.log(link)
+        } catch (err) {
+            console.log(err)
+        }
+    }
+  return (
+      <Container maxWidth={"sm"} component={"form"} onSubmit={submitHandler}>
+          <FormControl fullWidth={true} style={{marginTop: '30px'}}>
+              <InputLabel htmlFor="my-input">Instagram video URL</InputLabel>
+              <Input
+                  type={'url'}
+                  name={'url'}
+                  onSubmit={submitHandler}
+                  id="my-input"
+                  aria-describedby="my-helper-text"
+              />
+          </FormControl>
+
+          {link.downloadLink && (
+              <Box sx={{width: 280, margin: '0 auto'}}>
+                  <video controls autoPlay style={{width: '100%'}}>
+                      <source src={link.downloadLink} type={'video/mp4'}/>
+                  </video>
+              </Box>
+          )}
+      </Container>
+  );
+}
+
+export default App;
